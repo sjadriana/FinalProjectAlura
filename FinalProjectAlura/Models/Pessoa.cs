7f16883eefb572264132249ca6327d999f4c3af9
Adriana Silva De Jesus <a.silva.de.jesus@avanade.com>
2020-10-22 18:53:02 -0300

    feat(models):criando classe pessoa e controller

---
+++ FinalProjectAlura/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectAlura.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public string Nome { get; set; }
+    }
+}
